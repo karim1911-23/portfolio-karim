d6aa0cca9c54cb67ae004e37ed8ddd9485befc4b
karim <karim@gmail.com>
2025-01-27 15:31:20 +0100

    add theme

--- Frontend/src/Context/PortfolioContext.jsx
+++ Frontend/src/Context/PortfolioContext.jsx
@@ -4,7 +4,7 @@ import { useMediaQuery } from "react-responsive";
 const PortfolioContext = createContext();
 
 export default function PortfolioProvider({ children }) {
-  const [theme, settheme] = useState();
+  const [theme, settheme] = useState("e4004c");
   const [mode, setMode] = useState(localStorage.getItem("mode") || "light");
   const isMobile = useMediaQuery({ query: "(max-width: 618px)" });
   useEffect(() => {
