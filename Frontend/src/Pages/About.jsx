d53b19cb8be349e70110db7c13b9708963cbd9c9
karim <karim@gmail.com>
2025-01-27 15:39:41 +0100

    edit scrolly

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -170,7 +170,12 @@ export default function About() {
             overflowX: "scroll",
           }}
           id="toolsC"
-          className="tools"
+          className="tools max-h-[400px] overflow-y-auto
+  [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
         >
           <Tools />
         </div>
